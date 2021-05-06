99e7f4fe04b8b433740b480b6f4ae8f5cadf0252
Thomas <contact@thomas-george.com>
2021-05-06 18:58:59 +0200

    Initial Commit

---
+++ 100_Days_Project_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  100_Days_Project_1
+//
+//  Created by Thomas George on 06/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 100_Days_Project_1/_00_Days_Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  _00_Days_Project_1App.swift
+//  100_Days_Project_1
+//
+//  Created by Thomas George on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct _00_Days_Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
